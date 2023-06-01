f1941755e8520e2720311e7d24cee51048ced1f4
Patsy98 <smithalobo2698@gmail.com>
2023-06-01 13:19:16 +0530

    getelementbytag

--- dom.js
+++ dom.js
@@ -1,6 +1,8 @@
 //EXAMINE THE DOCUMENT OBJECT//
 //console.dir(document);
 
+//getelementbyid
+
 //console.log(document.getElementById('header-title'));
 //var headerTitle=document.getElementById('header-title');
 //console.log(headerTitle);
@@ -10,7 +12,9 @@
 
 //header.style.borderBottom = ' solid 3px #000'
 
-var element=document.getElementsByClassName('title');
+//getelement by classname
+
+/*var element=document.getElementsByClassName('title');
 console.log(element);
 console.log(element[0]);
 element[0].style.fontWeight ='bold';
@@ -23,4 +27,21 @@ element1[2].style.backgroundColor='green';
 for(var i=0;i<element1.length;i++){
     element1[i].style.fontWeight = 'bold';
 
+}*/
+
+//getelementbytagname
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[0]);
+li[0].style.fontWeight ='bold';
+li[0].style.color='green';
+
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold';
+
 }
+
+
+
